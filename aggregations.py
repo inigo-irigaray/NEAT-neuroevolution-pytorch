081f7c66cd796f039b43a53bd50f690731889631
Iñigo Irigaray <inigo.irigaray@gmail.com>
2020-01-13 12:43:18 +0100

    Update aggregations.py

--- aggregations.py
+++ aggregations.py
@@ -1,1 +1,15 @@
+from functools import reduce
+from operator import mul
 
+def sum_agg(inputs):
+  '''returns the sum of all input elements'''
+  return sum(inputs)
+
+def mul_agg(inputs):
+  '''returns the product of all input elements in the iterable'''
+  return reduce(mul, inputs, 1)
+
+str_to_aggregation = {
+    'sum': sum_agg,
+    'prod': prod_agg,
+}
