7f6c9e4bc1cbdad6b44b4f707741bbd2da85530c
Iñigo Irigaray <inigo.irigaray@gmail.com>
2020-01-13 16:05:47 +0100

    Update feed_forward_net.py

--- feed_forward_net.py
+++ feed_forward_net.py
@@ -1,38 +1,31 @@
 import torch
 import numpy as np
-import utils
 from .graphs import feed_forward
 from .activation_functions import sigmoid_act
   
 class FeedForwardNet():
-  def __init__(self, n_in, n_hid, n_out, in2hid, hid2hid, out2hid, in2out, hid2out, out2out, hid_responses, 
-               out_responses, hid_biases, out_biases, batch_size=1, use_current_acts=False, act=sigmoid_act, 
-               n_internal_steps=1, dtype=torch.float64):
+  def __init__(self, n_in, n_out, in2out, out2out, out_responses, out_biases, batch_size=1, 
+               act=sigmoid_act, dtype=torch.float64):
     
     self.n_in = n_in
-    self.n_hid = n_hid
     self.n_out = n_out
-    
-    if n_hidden > 0:
-      self.in2hid = utils.make_dense((n_hid, n_in), in2hid, dtype=dtype)
-      self.hid2hid = utils.make_dense((n_hid, n_hid), hid2hid, dtype=dtype)
-      self.out2hid = utils.make_dense((n_hid, n_out), out2hid, dtype=dtype)
-      self.hid2out = utils.make_dense((n_out, n_hid), hid2out, dtype=dtype)
-      self.hid_responses = torch.tensor(hid_responses, dtype=dtype)
-      self.hid_biases = torch.tensor(hid_biases, dtype=dtype)
-      
     self.in2out = torch.tensor((n_out, n_in)), in2out, dtype=dtype)
     self.out2out = torch.tensor((n_out, n_out), out2out, dtype=dtype)
     self.out_responses = torch.tensor(out_responses, dtype=dtype)
     self.out_biases = torch.tensor(out_biases, dtype=dtype)
-    self.use_current_acts = use_current_acts
     self.act = act
-    self.n_internal_steps = n_internal_steps
     self.dtype = dtype
     
-    self.reset(batch_size)
+    self._reset(batch_size)
     
+  def _reset(self, batch_size=1):
+    self.outputs = torch.zeros(batch_size, self.n_out, dtype=self.dtype)
   
-    
-    
-    
+  def activate(self, inputs):
+    with torch.no_grad():
+      if len(inputs[1]) != self.n_in:
+        raise RuntimeError("Expected %d inputs, got %d" % (self.n_in, len(inputs[1])))
+      
+      inputs = torch.tensor(inputs, dtype=self.dtype)
+      outputs_inputs = (self.in2out.mm(inputs.t()).t() + self.out2out.mm(self.outputs.t()).t())
+      
