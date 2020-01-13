518932638de625b59992391889cf013527780e9b
Iñigo Irigaray <inigo.irigaray@gmail.com>
2020-01-13 14:44:27 +0100

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -16,4 +16,20 @@ def required_for_outputs(inputs, outputs, connections):
   return required
   
 def feed_forward(inputs, outputs, connections):
-  
+  required = required_for_outputs(inputs, outputs, connections)
+  layers = []
+  s = set(inputs)
+  while True:
+    c = set(b for (a,b) in connections if a in s and b not in s)
+    t = set()
+    for n in c:
+      if n is required and all(a in s for (a,b) in connections if b==n):
+        t.add(n)
+        
+    if not t:
+      break
+      
+    layers.append(t)
+    s = s.union(t)
+    
+  return layers
