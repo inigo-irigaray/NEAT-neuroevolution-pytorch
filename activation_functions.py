d6c1e4f61c121d4244de965f207dfbd6319d4413
Iñigo Irigaray <inigo.irigaray@gmail.com>
2020-01-13 12:27:16 +0100

    Update activation_functions.py

--- activation_functions.py
+++ activation_functions.py
@@ -1,9 +1,41 @@
 import torch
 import torch.nn.functional as F
 
-def sigmoid(x):
+def sigmoid_act(x):
   return torch.sigmoid(x)
   
-def tanh(x):
+def tanh_act(x):
   return torch.tanh(x)
+
+def sin_act(x):
+  return torch.sin(x)
   
+def relu_act(x):
+  return F.relu(x)
+
+def leaky_relu_act(x):
+  return F.leay_relu(x)
+
+def elu_act(x):
+  return F.elu(x)
+
+def gaussian_act(x):
+  return torch.exp(- x**2)
+
+def identity_act(x):
+  return x
+
+def absolute_act(x):
+  return torch.abs(x)
+
+str_to_activation = {
+    'sigmoid': sigmoid_act,
+    'tanh': tanh_act,
+    'sin': sin_act,
+    'relu': relu_act,
+    'leaky_relu': leaky_relu_act,
+    'elu': elu_act,
+    'gaussian': gaussian_act,
+    'identity': identity_activation,
+    'abs': absolute_act,
+}
