ce9da145a836719e4edb4fd37b057a3f82db937c
Iñigo Irigaray <inigo.irigaray@gmail.com>
2020-01-14 10:53:48 +0100

    Update genome.py

--- genome.py
+++ genome.py
@@ -1,1 +1,11 @@
 """Implementation based on NEAT-Python genome.py"""
+
+import random
+import .activations
+import .aggregations
+import .config
+import .genes
+import .graphs
+
+class DefaultGenomeConfig:
+  
