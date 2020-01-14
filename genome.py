7a523d0d262e25ac889db0f3deb1ac424d81de57
Iñigo Irigaray <inigo.irigaray@gmail.com>
2020-01-14 16:25:53 +0100

    Update genome.py

--- genome.py
+++ genome.py
@@ -251,3 +251,46 @@ class DefaultGenome:
     if self.connections:
       key = random.choice(list(self.connections.keys()))
       del self.connections[key]
+
+  def distance(self, other, config):
+    node_distance = 0.0
+    if self.nodes or other.nodes:
+      disjoint_nodes = 0
+      for k2 in other.nodes.keys():
+        if k2 not in self.nodes:
+          disjoint_nodes += 1
+      for k1, n1 in self.nodes.items():
+        n2 = other.nodes.get(k1)
+        if n2 is None:
+          disjoint_nodes += 1
+        else:
+          node_distance += n1.distance(n2, config)
+      
+      max_nodes = max(len(self.nodes), len(other.nodes))
+      node_distance = (node_distance + (config.compatibility_disjoint_coefficient * disjoint_nodes)) / max_nodes
+      
+    connection_distance = 0.0
+    if self.connections or other.connections:
+      disjoint_connections = 0
+      for k2 in other.connections.keys():
+        if k2 not in self.connections.keys():
+          disjoint_connections += 1
+      for k1, c1 in self.connections.items():
+        c2 = other.connections.get(k1)
+        if c2 is None:
+          disjoint_connections += 1
+        else:
+          connection_distance += c1.distance(c2)
+          
+      max_connections = max(len(self.connections), len(other.connections))
+      connection_distance = (connection_distance + 
+                             (config.compatibility_disjoint_coefficient * disjoint_connections)) / max_connections
+      
+    distance = node_distance + connection_distance
+    return distance
+  
+  def size(self):
+    
+          
+  
+  
