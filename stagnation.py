0cabece84acf99b0ebf7bc6e8a8349a9245a8868
Iñigo Irigaray <inigo.irigaray@gmail.com>
2020-01-15 19:26:17 +0100

    Update stagnation.py

--- stagnation.py
+++ stagnation.py
@@ -1,1 +1,52 @@
 """Implementation based on NEAT-Python stagnation.py"""
+
+import sys
+from .config import ConfigParameter, DefaultClassConfig
+
+class DefaultStagnation(DefaultClassConfig):
+  def __init__(self, config, reporters):
+    self.stagnation_config = config
+    self.species_fitness_func = stat_functions.get(config.species_fitness_func)
+    if self.species_fitness_func is None:
+      raise RuntimeError("Unexpected species fitness function: {0!r}".format(config.species_fitness_func))
+      
+  @classmethod
+  def parse_config(cls, param_dic):
+    return DefaultClassConfig(param_dict, [ConfigParameter("species_fitness_func", str, "mean"),
+                                           ConfigParameter("max_stagnation", int, 15),
+                                           ConfigParameter("species_elitism", int, 0)])
+  
+  def update(self, species_set, generation):
+    species_data = []
+    for skey, s in species_set.species.items():
+      if s.fitness_history:
+        prev_fitness = max(s.fitness_history)
+      else:
+        prev_fitness = -sys.float_info.max
+        
+      s.fitness = self.species_fitness_func(s.get_fitnesses())
+      s.fitness_history.append(s.fitness)
+      s.adjusted_fitness = None
+      if prev_fitness is None or s.fitness > prev_fitness:
+        s.last_improved = generation
+        
+      species_data.append((skey, s))
+      
+    species_data.sort(key=lambda x: x[1].fitness)
+    
+    result = []
+    species_fitnesses = []
+    num_non_stagnant = len(species_data)
+    for idx, (skey, s) in enumerate(species_data):
+      stagnant_time = generation - s.last_improved
+      is_stagnant = False
+      if num_non_stagnant > self.stagnation_config.species_elitism:
+        is_stagnant = stagnant_time >= self.stagnation_config.max_stagnation
+      if (len(species_data) - idx) <= self.stagnation_config.species_elitism:
+        is_stagnant = False
+      if is_stagnant:
+        num_non_stagnant -= 1
+      result.append((skey, s, is_stagnant))
+      species_fitnesses.append(s.fitness)
+    
+    return result
