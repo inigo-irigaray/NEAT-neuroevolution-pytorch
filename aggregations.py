d1177cfb5908cae81cff6c73111ab08395cba84b
Iñigo Irigaray <inigo.irigaray@gmail.com>
2020-03-23 12:41:35 +0100

    Update aggregations.py

--- aggregations.py
+++ aggregations.py
@@ -1,17 +1,32 @@
-"""Implementation based on NEAT-Python aggregations.py"""
+"""Implementation based on NEAT-Python neat-python/aggregations.py:
+https://github.com/CodeReclaimers/neat-python/blob/master/neat/aggregations.py"""
 
 from functools import reduce
 from operator import mul
 
 def sum_agg(inputs):
-  '''returns the sum of all input elements'''
   return sum(inputs)
 
 def mul_agg(inputs):
-  '''returns the product of all input elements in the iterable'''
-  return reduce(mul, inputs, 1)
+    return reduce(mul, inputs, 1)
+
+def max_agg(inputs):
+    return max(inputs)
+
+def min_agg(inputs):
+    return min(inputs)
+
+def mean_agg(inputs):
+    return mean(inputs)
+
+def median_agg(inputs):
+  return median(inputs)
 
 str_to_aggregation = {
     'sum': sum_agg,
-    'prod': prod_agg,
+    'mul': mul_agg,
+    'max': max_agg,
+    'min': min_agg,
+    'mean': mean_agg,
+    'median': median,
 }
