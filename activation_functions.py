fe558f684f2ef5594f87565cbdc801a4e2b3c031
Iñigo Irigaray <inigo.irigaray@gmail.com>
2020-03-23 12:42:36 +0100

    Update activation_functions.py

--- activation_functions.py
+++ activation_functions.py
@@ -9,25 +9,25 @@ import torch.nn.functional as F
 """Selected scalars modify functions to have interesting properties around values -1, 0, 1 for x and y"""
 
 def sigmoid_act(x):
-  return torch.sigmoid(5.0 * x)
+    return torch.sigmoid(5.0 * x)
   
 def tanh_act(x):
-  return torch.tanh(2.5 * x)
+    return torch.tanh(2.5 * x)
 
 def sin_act(x):
-  return torch.sin(math.pi * x)
+    return torch.sin(math.pi * x)
   
 def relu_act(x):
-  return F.relu(x)
+    return F.relu(x)
 
 def gaussian_act(x):
-  return torch.exp(-5.0 * x**2)
+    return torch.exp(-5.0 * x**2)
 
 def identity_act(x):
-  return x
+    return x
 
 def absolute_act(x):
-  return torch.abs(x)
+    return torch.abs(x)
 
 str_to_activation = {
     'sigmoid': sigmoid_act,
