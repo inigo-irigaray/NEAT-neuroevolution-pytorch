07be9890279c684eceb5c2d2f43a0da010f960cd
Iñigo Irigaray <inigo.irigaray@gmail.com>
2020-03-24 13:28:03 +0100

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -1,54 +1,40 @@
-"""Implementation based on NEAT-Python graphs.py"""
+"""Implementation based on NEAT-Python neat-python/neat/graphs.py:
+https://github.com/CodeReclaimers/neat-python/blob/master/neat/graphs.py"""
+
+
+
 
 def required_for_outputs(inputs, outputs, connections):
-  required = set(outputs)
-  s = set(outputs)
-  while True:
-    t = set(a for (a,b) in connections if b in s and a not in s)
-    if not t:
-      break
+    required = set(outputs)
+    s = set(outputs)
+    while True:
+        t = set(a for (a,b) in connections if b in s and a not in s)
+        if not t:
+            break
       
-    layer_nodes = set(x for x in t if x not in inputs)
-    if not layer_nodes:
-      break
+        layer_nodes = set(x for x in t if x not in inputs)
+        if not layer_nodes:
+            break
       
-    required = required.union(layer_nodes)
-    s = s.union(t)
+        required = required.union(layer_nodes)
+        s = s.union(t)
   
-  return required
+    return required
 
 def creates_cycle(connections, test):
-  inp, out = test
-  if inp == out:
-    return True
+    inp, out = test
+    if inp == out:
+        return True
   
-  visited = {out}
-  while True:
-    n_added = 0
-    for a, b in connections:
-      if a in visited and b not in visited:
-        if b == inp:
-          return True
-        visited.add(b)
-        n_added += 1
-    if n_added == 0:
-      return False
-    
-"""def feed_forward(inputs, outputs, connections):
-  required = required_for_outputs(inputs, outputs, connections)
-  layers = []
-  s = set(inputs)
-  while True:
-    c = set(b for (a,b) in connections if a in s and b not in s)
-    t = set()
-    for n in c:
-      if n is required and all(a in s for (a,b) in connections if b==n):
-        t.add(n)
-        
-    if not t:
-      break
-      
-    layers.append(t)
-    s = s.union(t)
-    
-  return layers"""
+    visited = {out}
+    while True:
+        n_added = 0
+        for a, b in connections:
+            if a in visited and b not in visited:
+                if b == inp:
+                    return True
+                visited.add(b)
+                n_added += 1
+                
+        if n_added == 0:
+            return False
