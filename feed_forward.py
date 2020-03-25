19f53b4f0f5320f2ae324cd50fcfd164eedfbed0
Iñigo Irigaray <inigo.irigaray@gmail.com>
2020-03-25 12:54:04 +0100

    Update and rename feed_forward_net.py to feed_forward.py

--- feed_forward.py
+++ feed_forward.py
@@ -1,23 +1,27 @@
-import torch
 import numpy as np
-from .graphs import required_for_output
+import torch
+
 from .activation_functions import sigmoid_act
+from .graphs import required_for_output
+
+
+
   
 class FeedForwardNet():
-  def __init__(self, n_in, n_out, in2out, out_responses, out_biases, batch_size=1, act=sigmoid_act, dtype=torch.float64):
-    
-    self.n_in = n_in
-    self.n_out = n_out
-    self.in2out = torch.tensor((n_out, n_in)), in2out, dtype=dtype)
-    self.out_responses = torch.tensor(out_responses, dtype=dtype)
-    self.out_biases = torch.tensor(out_biases, dtype=dtype)
-    self.act = act
-    self.dtype = dtype
-    
-    self._reset(batch_size)
-    
-  def _reset(self, batch_size=1):
-    self.outputs = torch.zeros(batch_size, self.n_out, dtype=self.dtype)
+    def __init__(self, n_in, n_out, in2out, out_response, out_bias, batch_size=1, act=sigmoid_act, dtype=torch.float64):
+
+        self.n_in = n_in
+        self.n_out = n_out
+        self.in2out = torch.tensor((n_out, n_in)), in2out, dtype=dtype)
+        self.out_response = torch.tensor(out_responses, dtype=dtype)
+        self.out_bias = torch.tensor(out_biases, dtype=dtype)
+        self.act = act
+        self.dtype = dtype
+
+        self._reset(batch_size)
+
+    def _reset(self, batch_size=1):
+        self.outputs = torch.zeros(batch_size, self.n_out, dtype=self.dtype)
   
   def activate(self, inputs):
     with torch.no_grad():
