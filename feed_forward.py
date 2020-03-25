49a34d06aea6c676f5b9754d6c692165c8c780a3
Iñigo Irigaray <inigo.irigaray@gmail.com>
2020-03-25 13:24:07 +0100

    Update feed_forward.py

--- feed_forward.py
+++ feed_forward.py
@@ -7,84 +7,125 @@ from .graphs import required_for_output
 
 
   
-class FeedForwardNet():
-    def __init__(self, n_in, n_out, in2out, out_response, out_bias, batch_size=1, act=sigmoid_act, dtype=torch.float64):
-
-        self.n_in = n_in
-        self.n_out = n_out
-        self.in2out = torch.tensor((n_out, n_in)), in2out, dtype=dtype)
-        self.out_response = torch.tensor(out_responses, dtype=dtype)
-        self.out_bias = torch.tensor(out_biases, dtype=dtype)
-        self.act = act
+class FeedForwardNetwork():
+    def __init__(self, n_in, n_hid, n_out, in2hid, hid2out, in2out, hid_response, out_response, hid_bias,
+                 out_bias, batch_size=1, use_current_activs=False, activation=sigmoid_act,
+                 n_internal_steps=1, dtype=torch.float64):
+        
+        self.use_current_activs = use_current_activs
+        self.activation = activation
+        self.n_internal_steps = n_internal_steps
         self.dtype = dtype
+        
+        self.n_in = n_in
+        self.n_hid = n_hid
+        self.n_out = n_out
+        
+        if n_hid > 0:
+            self.in2hid = make_dense((n_hid, n_in), in2hid, dtype=dtype)
+            self.hid2out = make_dense((n_out, n_hid), hid2out, dtype=dtype)
+            
+        self.in2out = torch.tensor((n_out, n_in)), in2out, dtype=dtype)
+        
+        if n_hid > 0:
+            self.hid_response = torch.tensor(hid_response, dtype=dtype)
+            self.hid_bias = torch.tensor(hid_bias, dtype=dtype)
+            
+        self.out_response = torch.tensor(out_response, dtype=dtype)
+        self.out_bias = torch.tensor(out_bias, dtype=dtype)
 
-        self._reset(batch_size)
+        self.reset(batch_size)
 
-    def _reset(self, batch_size=1):
+    def reset(self, batch_size=1):
         self.outputs = torch.zeros(batch_size, self.n_out, dtype=self.dtype)
   
-  def activate(self, inputs):
-    with torch.no_grad():
-      if len(inputs[1]) != self.n_in:
-        raise RuntimeError("Expected %d inputs, got %d" % (self.n_in, len(inputs[1])))
+    def activate(self, inputs):
+        with torch.no_grad():
+            inputs = torch.tensor(inputs, dtype=self.dtype)
+            activs_for_output = self.activs
+            output_inputs = (self.in2out.mm(inputs.t()).t())    
+            
+            if self.n_hid > 0:
+                for _ in range(self.n_internal_steps):
+                    self.activs = self.activation(self.hid_response * (self.in2hid.mm(inputs.t()).t() + self.hid_bias))
+                if self.use_current_activs:
+                    activs_for_output = self.activs
+                output_inputs += self.hid2out.mm(activs_for_output.t()).t()
+            
+            self.outputs = self.activation(self.out_response * output_inputs + self.out_bias)
+            
+        return self.outputs
       
-      inputs = torch.tensor(inputs, dtype=self.dtype)
-      outputs_inputs = (self.in2out.mm(inputs.t()).t())
-      self.outputs = self.activation(self.output_responses * output_inputs + self.out_biases)
-      return self.outputs
+    @staticmethod
+    def create(genome, config, batch_size=1, activation=sigmoid_act, prune_empty=False,
+               use_current_activs=False, n_internal_steps=1):
+        genome_config = config.genome_config
+        required = required_for_output(genome_config.input_keys, genome_config.output_keys, genome.connections)
+        if prune_empty:
+            nonempty = {conn.key[1] for conn in genome.connections.values() if conn.enabled}.union(
+                set(genome_config.input_keys))
+            
+        input_keys = list(genome_config.input_keys)
+        hidden_keys = [k for k in genome.nodes.keys() if k not in genome_config.output_keys]
+        output_keys = list(genome_config.output_keys)
+        
+        hid_response = [genome.nodes[k].response for k in hidden_keys]
+        out_response = [genome.nodes[k].response for k in output_keys]
+        
+        hid_bias = [genome.nodes[k].bias for k in hidden_keys]
+        out_bias = [genome.nodes[k].bias for k in output_keys]
+        
+        if prune_empty:
+            for i, key in enumerate(output_keys):
+                if key not in nonempty:
+                    out_bias[i] = 0.0
+                    
+        n_in = len(input_keys)
+        n_hid = len(hidden_keys)
+        n_out = len(output_keys)
+        
+        input_key_to_idx = {k: i for i, k in enumerate(input_keys)}
+        hidden_key_to_idx = {k: i for i, k in enumerate(hidden_keys)}
+        output_key_to_idx = {k: i for i, k in enumerate(output_keys)}
+        
+        def key_to_idx(key):
+            if key in input_keys:
+                return input_key_to_idx[key]
+            elif key in hidden_keys:
+                return hidden_key_to_idx[key]
+            elif key in output_keys:
+                return output_key_to_idx[key]
+            
+        in2hid = ([], [])
+        hid2out = ([], [])
+        in2out = ([], [])
+        
+        for conn in genome.connections.values():
+            if not conn.enabled:
+                continue
+
+            i_key, o_key = conn.key
+            if o_key not in required and i_key not in required:
+                continue
+            if prune_empty and i_key not in nonempty:
+                print('Pruned {}'.format(conn.key))
+                continue
+
+            i_idx = key_to_idx(i_key)
+            o_idx = key_to_idx(o_key)
+
+            if i_key in input_keys and o_key in hidden_keys:
+                idxs, vals = in2hid
+            elif i_key in hidden_keys and o_key in output_keys:
+                idxs, vals = hid2out
+            elif i_key in input_keys and o_key in output_keys:
+                idxs, vals = in2out
+            else:
+                raise ValueError('Invalid connection from key {} to key {}'.format(i_key, o_key))
+
+            idxs.append((o_idx, i_idx))
+            vals.append(conn.weight)
       
-  @staticmethod
-  def create(genome, config, batch_size=1, activation=sigmoid_act, prune_empty=False):
-    genome_config = config.genome_config
-    required = required_for_output(genome_config.input_keys, genome_config.output_keys, genome_config.connections)
-    if prune_empty:
-      nonempty = {connection.key[1] for connection in genome.connections.values() if connection.enabled}.union(set(
-        genome_config.input_keys))
-      
-    in_keys = list(genome_config.input_keys)
-    out_keys = list(genome_config.output_keys)
-    out_responses = [genome.nodes[key].response for key in out_keys]
-    out_biases = [genome.nodes[key].bias for key in out_keys]
-    
-    if prune_empty:
-      for i, key in enumerate(out_keys):
-        if key not in nonempty:
-          out_biases[i] = 0.0
-          
-    n_in = len(in_keys)
-    n_out = len(out_leys)
-    
-    in2idx = {key: i for i, key in enumerate(in_keys)}
-    out2idx = {key: i for i, key in enumerate(out_keys)}
-    
-    def key2idx(key):
-      if key in in_keys:
-        return in2idx[key]
-      elif key in out_keys:
-        return out2idx[key]
-    
-    in2out = ([], [])
-    for connection in genome.connections.values():
-      if not connection.enabled:
-        continue
-      
-      in_key, out_key = connection.key
-      if out_key not in required and in_key not in required:
-        continue
-      
-      if prune_empty and in_key not in nonempty:
-        print("Pruned {} connection".format(connection.key))
-        continue
-      
-      in_idx = key2idx(in_key)
-      out_idx = key2idx(out_key)
-      
-      if in_key in in_keys and out_key in out_keys:
-        idxs, vals = in2out
-      else:
-        raise ValueError("Invalid connection from key {} to key {}."format(in_key, out_key))
-      
-      idxs.append((out_idx, in_idx))
-      vals.append(connection.weight)
-      
-    return FeedForwardNet(n_in, n_out, in2out, out_responses, out_biases, batch_size, activation)
+        return FeedForwardNetwork(n_in, n_hid, n_out, in2hid, hid2out, in2out, hid_response, out_response,
+                                  hid_bias, out_bias, batch_size=batch_size, activation=activation,
+                                  use_current_activs=use_current_activs, n_internal_steps=n_internal_steps)
